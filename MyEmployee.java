1bec95c1e1d0f30381ac486122997b153dbb7687
Anil Kumar <sendkumaranil@gmail.com>
2016-06-17 18:38:33 +0530

    MyEmployee Spring Bean Class

---
+++ MyEmployee.java
@@ -0,0 +1,70 @@
+package org.springexamples.bean;
+
+public class MyEmployee {
+	
+	private int empid;
+	private String empname;
+	private String birthDate;
+	private String joinDate;
+	private String idType;
+	private String idno;
+	private String mobileno;
+	private String email;
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEmpname() {
+		return empname;
+	}
+	public void setEmpname(String empname) {
+		this.empname = empname;
+	}
+	public String getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(String joinDate) {
+		this.joinDate = joinDate;
+	}
+	public String getIdType() {
+		return idType;
+	}
+	public void setIdType(String idType) {
+		this.idType = idType;
+	}
+	public String getIdno() {
+		return idno;
+	}
+	public void setIdno(String idno) {
+		this.idno = idno;
+	}
+	public String getMobileno() {
+		return mobileno;
+	}
+	public void setMobileno(String mobileno) {
+		this.mobileno = mobileno;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "MyEmployee: [empid=" + empid + ", empname=" + empname
+				+ ", birthDate=" + birthDate + ", joinDate=" + joinDate
+				+ ", idType=" + idType + ", idno=" + idno + ", mobileno="
+				+ mobileno + ", email=" + email + "]";
+	}
+
+}
