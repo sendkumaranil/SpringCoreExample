fbe120695bb7e3e9b342fd8a59e26881c43f7a45
Anil Kumar <sendkumaranil@gmail.com>
2016-06-17 18:37:59 +0530

    Employee Service class for CRUD Operation

---
+++ MyEmployeeService.java
@@ -0,0 +1,55 @@
+package org.springexample.service;
+
+import java.util.List;
+
+import org.springexamples.bean.MyEmployee;
+import org.springexamples.dao.MyEmployeeDao;
+import org.springexamples.daoImpl.MyEmployeeDaoImpl;
+
+public class MyEmployeeService {
+	
+	private MyEmployeeDao employeeObj;
+	
+	public void addEmployee(MyEmployee employee){
+		employeeObj.addEmployee(employee);
+	}
+	
+	public void updateEmployee(MyEmployee employee){
+		employeeObj.updateEmployee(employee);		
+	}
+	
+	public void deleteEmployee(MyEmployee employee){
+		employeeObj.deleteEmployee(employee);		
+	}
+	
+	public void fetchEmployee(){
+		
+		List<MyEmployee> employeeList=employeeObj.fetchEmployee();
+		System.out.println("Employee Records:");
+		int count=0;
+		if(employeeList !=null && !employeeList.isEmpty()){
+			for(MyEmployee employee:employeeList){
+				System.out.println("------------------Record["+(++count)+"]-----------------------------------------");
+					System.out.println("Employee Id:"+employee.getEmpid());
+					System.out.println("Employee Name:"+employee.getEmpname());
+					System.out.println("Employee Birth Date:"+employee.getBirthDate());
+					System.out.println("Employee join Date"+employee.getJoinDate());
+					System.out.println("Employee Id Type:"+employee.getIdType()+" Employee Id No:"+employee.getIdno());
+					System.out.println("Employee Mobile No:"+employee.getMobileno());
+					System.out.println("Employee email:"+employee.getEmail());
+				System.out.println("-------------------------------------------------------------------");
+			}
+		}else{
+			System.out.println("-----------Record Not Found in Database---------------");
+		}
+	}
+
+	public MyEmployeeDao getEmployeeObj() {
+		return employeeObj;
+	}
+
+	public void setEmployeeObj(MyEmployeeDao employeeObj) {
+		this.employeeObj = employeeObj;
+	}
+	
+}
